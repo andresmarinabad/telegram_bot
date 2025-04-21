86bf47ffec075d122132d0ad26fc0b59c54b32b1
Andres M <andres.marin.abad+git@gmail.com>
2025-04-22 00:28:31 +0200

    Add first version only code

---
+++ bot.py
@@ -0,0 +1,33 @@
+import os
+import json
+import random
+from telegram.ext import Updater, MessageHandler, Filters
+
+
+with open("respuestas.json", "r", encoding="utf-8") as f:
+    RESPUESTAS = json.load(f)
+
+def responder(update, context):
+    if not update.message or not update.message.text:
+        return
+
+    mensaje = update.message.text.lower()
+    for palabra, posibles_respuestas in RESPUESTAS.items():
+        if palabra in mensaje:
+            respuesta = random.choice(posibles_respuestas)
+            update.message.reply_text(
+                respuesta,
+                reply_to_message_id=update.message.message_id
+            )
+            break  # Solo responde a la primera palabra que coincida
+
+def main():
+    TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, responder))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
